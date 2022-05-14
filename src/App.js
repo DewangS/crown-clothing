8a6bd6e0f5c9ccc27f39177c3f42564d2cab6642
30053320 <30053320@westernsydney.edu.au>
2022-05-14 20:15:01 +1000

    created authentication component and signin page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {Route, Routes} from 'react-router-dom'
 import Home from './routes/home/home.component'
-import SignIn from './routes/sign-in/sign-in.component'
+import Authentication from './routes/authentication/authentication.component'
 import Navigation from './routes/navigation/navigation.component'
 
 const Shop = () => {
@@ -15,7 +15,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='home' element={<Home />} />
         <Route path='shop' element={<Shop/>}/>
-        <Route path='sign-in' element={<SignIn />}/>
+        <Route path='authentication' element={<Authentication />}/>
       </Route>
     </Routes>
   );
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -4,7 +4,8 @@ import {getAuth,
     signInWithRedirect, 
     signInWithPopup, 
     GoogleAuthProvider,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth'
 
 import {
@@ -77,4 +78,13 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
     return;
     
     return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  
+  console.log('+++ email and password are '+email+' '+password)
+  if(!email || !password)
+  return;
+  
+  return await signInWithEmailAndPassword(auth, email, password)
 }
